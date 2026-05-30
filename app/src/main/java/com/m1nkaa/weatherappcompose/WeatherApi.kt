2240e4c38b651327c23e8513da5c7a6cbb98c1cd
M1nkaa <polina.jasko@bk.ru>
2026-05-30 22:40:18 +0300

    feat: add Retrofit API and response models

---
+++ app/src/main/java/com/m1nkaa/weatherappcompose/WeatherApi.kt
@@ -0,0 +1,15 @@
+package com.m1nkaa.weatherappcompose
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("weather")
+    suspend fun getWeather(
+        @Query("q") city: String,
+        @Query("appid") apiKey: String,
+        @Query("units") units: String = "metric",
+        @Query("lang") lang: String = "en"
+    ): WeatherResponse
+}
